45b1647210eee8059b325b09261b09bee670dd6e
rajnish003 <raj003se.com>
2024-08-18 14:14:03 +0530

    add header in home section

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const HeroSection = () => {
   return (
     <section className="bg-blue-500 text-white py-16">
-      <div className="container mx-auto text-center">
+
+      <div className="container mx-auto text-center ">
+      <h1 className='text-yellow-500 text-4xl md:text-4xl font-bold mb-4 bg-[#141512] py-2 rounded-md w-[75%] m-auto'>Raj Software Solution</h1>
         <h2 className="text-3xl md:text-4xl font-bold mb-4">Empowering Your Business with Cutting-Edge Software Solutions</h2>
         <p className="text-lg mb-8">We provide innovative solutions to help your business succeed in the digital age.</p>
         <a href="contact" className="bg-yellow-500 text-black px-6 py-3 rounded-full font-semibold hover:bg-yellow-400">Get in Touch</a>
